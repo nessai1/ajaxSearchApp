14c556d11a7756594fe458789a0b79fee817d9b0
Daniil Verevkin <werowkin@gmail.com>
2021-08-12 09:22:00 +0200

    create search comments of posts

--- js/app.js
+++ js/app.js
@@ -39,37 +39,115 @@ function searchHandler(e)
     }
 }
 
+function getNotFound()
+{
+    return document.getElementById("notFound");
+}
+
+
+function clearNotFound()
+{
+    let notFound = getNotFound();
+    if (notFound === null)
+    {
+        return;
+    }
+
+    notFound.remove();
+}
+
+function setNotFound()
+{
+    if (getNotFound() !== null)
+    {
+        return;
+    }
+
+    let notFound = document.createElement('div');
+    notFound.id = 'notFound';
+    notFound.className = 'notFoundBlock';
+
+    let notFoundText = document.createElement('h3')
+    notFoundText.className = 'notFoundBlock__text';
+    notFoundText.textContent = 'Ничего не найдено :(';
+
+    notFound.appendChild(notFoundText);
+
+    let app = document.getElementById('app');
+    app.appendChild(notFound);
+}
+
+function refreshSearchList()
+{
+    clearSearchList();
+    clearNotFound();
+    setSearchList();
+}
+
+function getSearchList()
+{
+    return document.getElementById('searchList');
+}
+
+function setSearchList()
+{
+    if (getSearchList() !== null)
+    {
+        return;
+    }
+
+    let searchList = document.createElement('div');
+    searchList.id = 'searchList';
+    searchList.className = 'searchResult';
+    let app = document.getElementById('app');
+    app.appendChild(searchList);
+}
+
 // searchList.remove();
 
 function clearSearchList()
 {
-    for (let i = searchList.childNodes.length-1; i >= 0; i--)
+    let searchList = getSearchList();
+    if (searchList === null)
     {
-        console.log("removing child....", searchList.lastElementChild);
-        searchList.removeChild(searchList.lastElementChild);
+        return;
+    }
+
+    searchList.remove();
+}
+
+function fillSearchList(searchItems)
+{
+    refreshSearchList();
+    searchList = getSearchList();
+    for (let i = 0; i < searchItems.length; i++)
+    {
+        searchList.append(createSearchItem({title: searchItems[i].email, desc: searchItems[i].body}));
     }
 }
 
 async function getSearchResult(requestValue)
 {
+    let searchList = getSearchList();
     if (requestValue === '')
     {
+        clearSearchList();
+        clearNotFound();
+        return;
+    }
 
-    }
-    else
-    {
-        searchList.append(createSearchItem({title: "Good job", desc: "We have some good news"}));
-        searchList.append(createSearchItem({title: "Hello world :))", desc: "We have some good news"}));
-    }
-    let searchCommentsUrl = searchUrl + '/' + requestValue + '/comments';
+    let searchCommentsUrl = searchUrl + '/posts/' + requestValue + '/comments';
     let response = await fetch(searchCommentsUrl);
     let commits = await response.json();
-    console.log(commits);
+    if (commits.length === 0)
+    {
+        clearSearchList();
+        setNotFound();
+        return;
+    }
+
+    fillSearchList(commits);
 }
 
 
-let searchList = document.getElementById('searchList');
-
-searchList.append(createSearchItem({title: "Good job", desc: "We have some good news"}));
-searchList.append(createSearchItem({title: "Hello world :))", desc: "We have some good news"}));
 
